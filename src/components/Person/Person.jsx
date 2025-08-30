61eaed33dfdce4d2069317939f06f374b517f7f9
tvk777 <klimenko.tv7@gmail.com>
2025-08-30 16:45:57 +0300

    fix partnerMessage

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,16 @@
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const { name, age, isMarried, sex, partnerName } = person;
+
+  const partnerRole = sex === 'f' ? 'husband' : 'wife';
+  const partnerMessage = isMarried
+    ? `${partnerName} is my ${partnerRole}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age !== undefined && <p className="Person__age">I am {age}</p>}
-      <p className="Person__partner">
-        {isMarried ? (
-          <p className="Person__partner">
-            {partnerName} is my {sex === 'f' ? 'husband' : 'wife'}
-          </p>
-        ) : (
-          'I am not married'
-        )}
-      </p>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerMessage}</p>
     </section>
   );
 };
